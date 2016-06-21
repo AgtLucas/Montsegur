f1e23c606524ff46fc75e8bb025455677135ce5a
Lucas <lucas@agtlucas.com>
2016-06-21 20:21:38 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+import '../css/main.css'
+
 class App extends Component {
   render () {
     return <h1>Montsegur</h1>
